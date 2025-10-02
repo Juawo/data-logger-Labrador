aa02ef8ea5edcc988dd6700292ddc8bb0042ef16
Juawo <joaopedrojp2703@gmail.com>
2025-10-02 11:29:52 -0300

    refactor save_data function to append data instead of overwriting

--- save-data.py
+++ save-data.py
@@ -1,9 +1,5 @@
-def save_data(path, data):
-    file = open(path, "w")
-
-    for i in range(3):
-        data = input("Digite um valor")
-        file.write(data)
-        file.write("\n")
-
+def save_data(path,data):
+    file = open(path, "a")
+    file.write(str(data))
+    file.write("\n")
     file.close()
