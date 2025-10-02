aeaa6d7c5fb0c6917e2f10f312291b883e65444d
Juawo <joaopedrojp2703@gmail.com>
2025-10-02 11:06:18 -0300

    add save-data module

--- save-data.py
+++ save-data.py
@@ -1,1 +1,9 @@
-print("save-data-SD-Card")
+def save_data(path, data):
+    file = open(path, "w")
+
+    for i in range(3):
+        data = input("Digite um valor")
+        file.write(data)
+        file.write("\n")
+
+    file.close()
